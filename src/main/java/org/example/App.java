43ad7ffa68de90d4521a1ff7b31b37f3d930a1e9
Felizta <feliztaaa@gmail.com>
2024-09-21 20:36:09 +0200

    Snyggast till koden och lagt till metoden för Sortera

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,27 +1,30 @@
 package org.example;
 
+import java.sql.SQLOutput;
 import java.util.*;
 
 public class App {
+    static Scanner in = new Scanner(System.in);
+    //Array för att lagra elpriserna
+    static final int hours = 24;
+    static final int[] elpriser = new int[hours];
+    //static int[] elpriser = new int[24];
+    static char alternativ;
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        //Array för att lagra elpriserna
-        int[] elpriser = new int[24];
-        char alternativ;
 
 
-
-        while(true) {
+        while (true) {
             //Skapar menyn som en String
             String menu = """
-        Elpriser
-        ========
-        1. Inmatning
-        2. Min, Max och Medel
-        3. Sortera
-        4. Bästa Laddningstid (4h)
-        e. Avsluta
-        """;
+                    Elpriser
+                    ========
+                    1. Inmatning
+                    2. Min, Max och Medel
+                    3. Sortera
+                    4. Bästa Laddningstid (4h)
+                    e. Avsluta
+                    """;
             //Omvandlar menyn till en Byte array
             byte[] menuBytes = menu.getBytes();
             System.out.print(new String(menuBytes));
@@ -29,13 +32,17 @@ public class App {
             //Tar emot användarens input
             alternativ = in.next().charAt(0);
 
-            switch(alternativ) {
+            switch (alternativ) {
                 case '1': //Inmatning
-                    inmatning(in, elpriser);
+                    inmatning();
                     break;
 
                 case '2':
-                    statistik(elpriser);
+                    statistik();
+                    break;
+
+                case '3':
+                    sortera();
                     break;
 
                 case 'e':
@@ -51,29 +58,26 @@ public class App {
         }
     }
 
-    private static void inmatning(Scanner in, int[] elpriser) {
-        // Återställer alla priser till noll
-        Arrays.fill(elpriser, 0);
-
+    private static void inmatning() {
+        System.out.print("Ange elpriser för varje timma på dygnet i hela öre/kWh:\r\n");
 
         //Loopar igenom alla dygnets timmar
-        for(int hour = 0; hour < 24; hour++) {
-            while(true){
-                if (in.hasNextInt()){
-                    int pris = in.nextInt();
-                    elpriser[hour] = pris;
-                    break;
-                } else {
-                    System.out.println("Ikke godkänd inmatning. Ange öre i heltal.");
-                    in.next();
-                }
+        for (int hour = 0; hour < 24; hour++) {
+            System.out.print("Skiv elpris för varje timma " + hour + "-" + (hour + 1) + ": ");
+            if (in.hasNextInt()) {
+                int pris = in.nextInt(); //Läser in varje elpris
+                elpriser[hour] = pris;
+            } else {
+                System.out.println("Ikke godkänd inmatning. Ange öre i heltal.");
+                in.next();
+                hour--;
             }
         }
     }
 
-    private static void statistik(int[] elpriser) {
+    private static void statistik() {
         if (elpriser[0] == 0 && ingaPriser(elpriser)) {
-            System.out.println("Inmatning inte gjord än");
+            System.out.println("Inmatning inte gjord än\r\n");
         }
 
         int minPris = elpriser[0];
@@ -83,11 +87,11 @@ public class App {
         int totalPris = 0;
 
         for (int hour = 0; hour < 24; hour++) {
-            if(elpriser[hour] < minPris) {
+            if (elpriser[hour] < minPris) {
                 minPris = elpriser[hour];
                 minTimmar = hour;
             }
-            if(elpriser[hour] > maxPris) {
+            if (elpriser[hour] > maxPris) {
                 maxPris = elpriser[hour];
                 maxTimmar = hour;
             }
@@ -103,13 +107,28 @@ public class App {
                 Lägsta pris: %02d-%02d, %d öre/kWh
                 Högsta pris: %02d-%02d, %d öre/kWh
                 Medelpris: %s öre/kWh
-                """,
-                minTimmar, (minTimmar + 1) % 24, minPris,
-                maxTimmar, (maxTimmar + 1) % 24, maxPris,
-                medelPrisFormatted);
+                """, minTimmar, (minTimmar + 1) % 24, minPris, maxTimmar, (maxTimmar + 1) % 24, maxPris, medelPrisFormatted);
 
         // Display the results with desired formatting
         System.out.println(response);
+    }
+
+    public static void sortera() {
+        int[] sortCosts = Arrays.copyOf(elpriser, elpriser  .length);
+        Arrays.sort(sortCosts);
+
+        boolean[] written = new boolean[hours];
+
+        System.out.print("Dyrast till billigast tider:\n");
+        for (int i = sortCosts.length - 1; i >= 0; i--) {
+            for (int j = 0; j < elpriser.length; j++) {
+                if (elpriser[j] == sortCosts[i] && !written[j]) {
+                    System.out.printf("%02d-%02d %d öre\n", j, j + 1, elpriser[j]);
+                    written[j] = true;
+                    break;
+                }
+            }
+        }
     }
 
     private static boolean ingaPriser(int[] elpriser) {
@@ -120,4 +139,5 @@ public class App {
         }
         return true;
     }
+
 }
